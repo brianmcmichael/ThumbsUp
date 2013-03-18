3510f593ddf2225ff968cda266c389f1521ee687
Brian L. McMichael <brian@brianmcmichael.com>
2013-03-18 18:23:28 -0400

    Get posts.

--- web/classes/Rate.php
+++ web/classes/Rate.php
@@ -4,7 +4,8 @@
  * Date: 3/18/13
  * Time: 5:31 PM, 3/18/13, 2013
  */
-class Rate {
+class Rate
+{
     private $objDb = null;
 
     private $_dbhost = 'localhost';
@@ -18,12 +19,13 @@ class Rate {
     public $_user;
 
 
-
-    public function __construct($user = null) {
+    public function __construct($user = null)
+    {
         $this->_user = !empty($user) ? $user : getenv('REMOTE_ADDR');
     }
 
-    private function connect() {
+    private function connect()
+    {
         try {
             $this->objDb = new PDO("mysql:host={$this->_dbhost};dbname={$this->_dbname}",
                 $this->_dbuser,
@@ -33,9 +35,24 @@ class Rate {
                 )
             );
             $this->objDb->exec("SET CHARACTER SET utf8");
-        }   catch(PDOException $e) {
+        } catch (PDOException $e) {
             echo $e->getMessage();
             exit();
         }
+    }
+
+    public function getPosts()
+    {
+        if ($this->objDb == null) {
+            $this->connect();
+        }
+        $sql = "SELECT *,
+                DATE FORMAT('date', '%d/%m/%Y' AS
+                'date_formatted'
+                FROM '{this->_table_1}'
+                WHERE 'active' = 1
+                ORDER BY 'date' DESC";
+        $statement = $this->objDb->query($sql);
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
     }
 }
